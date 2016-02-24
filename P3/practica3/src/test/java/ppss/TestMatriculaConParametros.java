59399cfbc421e44f11bfba51183e44365da051a4
Cristina Rivera Baydal <crb13@alu.ua.es>
2016-02-24 14:05:33 +0100

    Práctica 3 Ejercicio 1B

--- P3/practica3/src/test/java/ppss/TestMatriculaConParametros.java
+++ P3/practica3/src/test/java/ppss/TestMatriculaConParametros.java
@@ -5,6 +5,8 @@
  */
 package ppss;
 
+import java.util.Arrays;
+import java.util.Collection;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -18,14 +20,23 @@ import org.junit.runners.Parameterized;
  *
  * @author ppss
  */
+@RunWith(Parameterized.class)
 public class TestMatriculaConParametros {
-    
+    @Parameterized.Parameters(name = "Caso C{index}: calculaTasaMatricula({0}, {1}, {2}) = {3}")
+    public static Collection<Object[]> data(){
+        return Arrays.asList(new Object[][]{
+            {20, false, true, 2000},
+            {70, false, true, 250},
+            {20, true, true, 250},
+            {20, false, false, 500},
+            {60, false, true, 400}
+        });
+    }
     int edad;
     boolean familiaNumerosa;
     boolean repetidor;
     float resultadoReal, resultadoEsperado;
     Matricula mat= new Matricula();
-
     
     @BeforeClass
     public static void setUpClass() {
@@ -43,9 +54,20 @@ public class TestMatriculaConParametros {
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    public TestMatriculaConParametros(int edad, boolean familiaNumerosa, boolean repetidor, float resultadoEsperado){
+        this.edad = edad;
+        this.familiaNumerosa = familiaNumerosa;
+        this.repetidor = repetidor;
+        this.resultadoEsperado = resultadoEsperado;
+    }
+    @Test
+    public void testMatricula() {
+        
+        resultadoReal = mat.calculaTasaMatricula(edad,familiaNumerosa,repetidor);
+        //el tercer parámetro del método Assert.assertEquals es necesario si estamos comparando "floats"
+        //en este caso el método devuelve cierto si:
+        //resultadoEsperado = resultadoReal +/- 0.002
+        assertEquals(resultadoEsperado, resultadoReal, 0.002f);
+    }
+    
 }
