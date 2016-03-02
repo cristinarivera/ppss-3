9ce2616644b5958429d7740fc2c156061675fb35
Cristina Rivera Baydal <crb13@alu.ua.es>
2016-03-02 18:59:35 +0100

    Práctica 5 Ejercicio 1

---
+++ Nueva Carpeta/gestorllamadas/src/main/java/ppss/GestorLlamadas.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ppss;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author Cristina Rivera Baydal
+ */
+public class GestorLlamadas {
+    
+    static double TARIFA_NOCTURNA=10.5;
+    static double TARIFA_DIURNA=20.8;
+    public int getHoraActual() {
+        Calendar c = Calendar.getInstance();
+        int hora = c.get(Calendar.HOUR);
+    return hora;
+    }
+
+
+    public double calculaConsumo(int minutos) {
+        int hora = getHoraActual();
+        if(hora < 8 || hora > 20) {
+            return minutos * TARIFA_NOCTURNA;
+        } else {
+        return minutos * TARIFA_DIURNA;
+        }
+    }
+    
+    
+}
---
+++ Nueva Carpeta/gestorllamadas/src/test/java/ppss/TestGestorLlamadas.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ppss;
+
+import java.util.Calendar;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+/**
+ *
+ * @author ppss
+ */
+public class TestGestorLlamadas extends GestorLlamadas{
+    double resultadoEsperado;
+    double resultadoReal;
+    int minutos;
+    GestorLlamadas gl = new GestorLlamadas();
+    private int hora; 
+    @Override
+    public int getHoraActual() {
+        return hora;
+    }
+    public void setHoraActual(int h){
+        this.hora = h;
+    }
+    @Test
+    public void TestCalculaConsumoC1() {
+        minutos = 10;
+        resultadoEsperado = 208.0;
+        setHoraActual(15);
+        resultadoReal = gl.calculaConsumo(minutos);
+        assertEquals(resultadoEsperado, resultadoReal, 0.01);
+        
+    }
+    @Test
+    public void TestCalculaConsumoC2() {
+        minutos = 10;
+        resultadoEsperado = 105.0;
+        setHoraActual(22);
+        resultadoReal = gl.calculaConsumo(minutos);
+        assertEquals(resultadoEsperado, resultadoReal, 0.01);
+    }
+}
